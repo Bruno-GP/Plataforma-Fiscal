e671e47fa4ef1f11a3e2dd72861fc73b54055569
Bruno-GP <suportegestaopraticacont@gmail.com>
2026-01-23 16:02:19 -0300

    Correção erro 405

--- API/app/main.py
+++ API/app/main.py
@@ -5,6 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router as api_router
 
@@ -12,6 +13,14 @@ app = FastAPI(
     title="API - Agente Extrator NFe",
     version="0.1.0",
     description="API para processar XML de NFe e preparar dados para relatórios executivos."
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api")

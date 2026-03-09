b76d6cbfd0da94fddb8d8ad133a668d21e207eca
Bruno-GP <suportegestaopraticacont@gmail.com>
2026-03-09 14:34:50 -0300

    Passado analise de vendas para a pasta pages

--- Painel/src/pages/AnaliseVendas.tsx
+++ Painel/src/pages/AnaliseVendas.tsx
@@ -4,17 +4,17 @@ import { useQuery } from '@tanstack/react-query';
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
-import { Header } from '../components/Header';
-import { RankingCard } from '../components/RankingCard';
-import { StatCard } from '../components/StatCard';
-import { SalesRegionCityMap } from '../components/SalesRegionCityMap';
-import { EvolucaoChart } from '../components/EvolucaoChart';
+import { Header } from './components/Header';
+import { RankingCard } from './components/RankingCard';
+import { StatCard } from './components/StatCard';
+import { SalesRegionCityMap } from './components/SalesRegionCityMap';
+import { EvolucaoChart } from './components/EvolucaoChart';
 
 import { fetchNfeKpis, fetchNfeKpisComparativoAtual, parseDecimal } from '@/services/nfe';
 import { useAuth } from '@/contexts/AuthContext'
 import { fetchSpedKpis } from '@/services/sped';
 // import { useChat } from '@/contexts/ChatContext';
-import { monthLabels } from '../faturamento/utils/utils';
+import { monthLabels } from './faturamento/utils/utils';
 
 const formatCurrency = (value: number) =>
   new Intl.NumberFormat('pt-BR', {

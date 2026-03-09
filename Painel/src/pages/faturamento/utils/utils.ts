6df33311a813a2c84b00f5d11bbc81898648c32b
Bruno-GP <suportegestaopraticacont@gmail.com>
2026-03-09 14:47:58 -0300

    Tirado a pagina de faturamento

--- Painel/src/pages/faturamento/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export const formatCurrency = (value: number) => {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(value);
-};
-
-export const monthLabels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
